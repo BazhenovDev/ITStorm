b27a497c1f84c784edd60d4427744e75b813ce96
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2025-08-11 18:02:57 +0300

    create custom slider

--- frontend/src/app/views/main/main.component.ts
+++ frontend/src/app/views/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SliderType} from "../../../types/slider.type";
+import {SliderService} from "../../shared/services/slider.service";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  sliders: SliderType[] = [];
+  currentSlideIndex: number = 0;
+  currentSlide: SliderType = {
+    image: '',
+    pretitle: '',
+    title: '',
+  };
+
+  constructor(private sliderService: SliderService) { }
 
   ngOnInit(): void {
+    this.sliders = this.sliderService.getSliders();
+    this.currentSlide = this.sliders[this.currentSlideIndex];
   }
 
+  goToNextSlide(): void {
+    this.currentSlideIndex++;
+    if (this.currentSlideIndex === this.sliders.length) {
+      this.currentSlideIndex = 0;
+      this.currentSlide = this.sliders[this.currentSlideIndex];
+    }
+    this.currentSlide = this.sliders[this.currentSlideIndex];
+  }
+
+  goToPrevSlide(): void {
+    this.currentSlideIndex--;
+    if (this.currentSlideIndex < 0) {
+      this.currentSlideIndex = this.sliders.length - 1;
+      this.currentSlide = this.sliders[this.currentSlideIndex];
+    }
+    this.currentSlide = this.sliders[this.currentSlideIndex];
+  }
+
+  goToSlide(index: number): void {
+    this.currentSlideIndex = index;
+    this.currentSlide = this.sliders[this.currentSlideIndex];
+  }
 }
