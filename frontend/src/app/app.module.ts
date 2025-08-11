321caf78afcaf05091eeb3dc84836c0579824a4d
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2025-08-11 14:40:33 +0300

    create owl carousel

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import {LayoutComponent} from "./shared/layout/layout.component";
 import {HeaderComponent} from "./shared/layout/header/header.component";
 import {FooterComponent} from "./shared/layout/footer/footer.component";
 import { MainComponent } from './views/main/main.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CarouselModule} from "ngx-owl-carousel-o";
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { MainComponent } from './views/main/main.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CarouselModule,
     SharedModule,
     AppRoutingModule
   ],
--- frontend/src/app/views/main/main.component.ts
+++ frontend/src/app/views/main/main.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-main',
+  selector: 'main-component',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
@@ -10,6 +11,26 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: false
+  }
+
+
+
 
 }
